d9389efff26be7e8c96953a8ff17093f563c943d
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2023-08-21 21:44:45 +0700

    video 50

--- src/components/users/users.table.tsx
+++ src/components/users/users.table.tsx
@@ -1,6 +1,47 @@
+import { useEffect, useState } from 'react';
 import '../../styles/users.css';
 
 const UsersTable = () => {
+
+
+    useEffect(() => {
+        //update
+        console.log(">>> check useEffect")
+        getData()
+    }, [])
+
+    const getData = async () => {
+        const res = await fetch(
+            "http://localhost:8000/api/v1/auth/login",
+            {
+                method: "POST",
+                body: JSON.stringify({
+                    username: "hoidanit@gmail.com",
+                    password: "123456"
+                }),
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            })
+
+        const data = await res.json();
+
+        const access_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0b2tlbiBsb2dpbiIsImlzcyI6ImZyb20gc2VydmVyIiwiX2lkIjoiNjRkMWM0OTYxNmE3Nzc2YjExOThiZjcyIiwiZW1haWwiOiJob2lkYW5pdEBnbWFpbC5jb20iLCJhZGRyZXNzIjoiVmlldE5hbSIsImlzVmVyaWZ5Ijp0cnVlLCJuYW1lIjoiSSdtIEjhu49pIETDom4gSVQiLCJ0eXBlIjoiU1lTVEVNIiwicm9sZSI6IkFETUlOIiwiaWF0IjoxNjkyNjI4NDg3LCJleHAiOjE2OTI2ODg0ODd9.hq9bsgH_-vy64E6Q86wgYXfZtrtUgPzv3_HXUdieegU"
+
+        const res1 = await fetch(
+            "http://localhost:8000/api/v1/users/all",
+            {
+                headers: {
+                    'Authorization': `Bearer ${access_token}`,
+                    "Content-Type": "application/json",
+                },
+            })
+
+        const data1 = await res1.json();
+        console.log(">>> check data 1: ", data1)
+    }
+
+    console.log(">>> check render")//mounting
     return (
         <div>
             <h2>HTML Table</h2>
