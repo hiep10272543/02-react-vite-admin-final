f82d2e5d603f16efd9bd11f3d78f960583af2e6b
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2023-08-23 20:27:29 +0700

    video 57

--- src/components/users/users.table.tsx
+++ src/components/users/users.table.tsx
@@ -19,22 +19,7 @@ const UsersTable = () => {
     }, [])
 
     const getData = async () => {
-        const access_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0b2tlbiBsb2dpbiIsImlzcyI6ImZyb20gc2VydmVyIiwiX2lkIjoiNjRkMWM0OTYxNmE3Nzc2YjExOThiZjcyIiwiZW1haWwiOiJob2lkYW5pdEBnbWFpbC5jb20iLCJhZGRyZXNzIjoiVmlldE5hbSIsImlzVmVyaWZ5Ijp0cnVlLCJuYW1lIjoiSSdtIEjhu49pIETDom4gSVQiLCJ0eXBlIjoiU1lTVEVNIiwicm9sZSI6IkFETUlOIiwiaWF0IjoxNjkyNzE0MDI1LCJleHAiOjE2OTI3NzQwMjV9.xPIzeZQCutmM4W9ELtD4urhAmv2lJWUZaAkK2DeP5B4"
-
-        const res1 = await fetch(
-            "http://localhost:8000/api/v1/auth/login",
-            {
-                headers: {
-                    'Authorization': `Bearer ${access_token}`,
-                    "Content-Type": "application/json",
-                },
-                method: "POST",
-                body: JSON.stringify({
-                    email: "hoidanit@gmail.com",
-                    password: "123456"
-                })
-            })
-
+        const access_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0b2tlbiBsb2dpbiIsImlzcyI6ImZyb20gc2VydmVyIiwiX2lkIjoiNjRkMWM0OTYxNmE3Nzc2YjExOThiZjcyIiwiZW1haWwiOiJob2lkYW5pdEBnbWFpbC5jb20iLCJhZGRyZXNzIjoiVmlldE5hbSIsImlzVmVyaWZ5Ijp0cnVlLCJuYW1lIjoiSSdtIEjhu49pIETDom4gSVQiLCJ0eXBlIjoiU1lTVEVNIiwicm9sZSI6IkFETUlOIiwiaWF0IjoxNjkyNzk2NzcwLCJleHAiOjE2OTI4NTY3NzB9.NpyLHRmn_IjJxxWdhrAH7TsnZtfs7-cPnJ2TY3cOPwM"
 
         const res = await fetch(
             "http://localhost:8000/api/v1/users/all",
