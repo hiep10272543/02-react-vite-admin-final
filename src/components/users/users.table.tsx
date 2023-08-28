da7fe0b6b84842f0a0fe5a3d0b3dc18b432c8ebd
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2023-08-28 22:33:01 +0700

    video 67.1

--- src/components/users/users.table.tsx
+++ src/components/users/users.table.tsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from 'react';
 import { Table, Button, Modal, Input, notification } from 'antd';
 import type { ColumnsType } from 'antd/es/table';
 import { PlusOutlined } from '@ant-design/icons';
+import CreateUserModal from './create.user.modal';
+import UpdateUserModal from './update.user.modal';
 
 interface IUsers {
     _id: string;
@@ -15,15 +17,10 @@ const UsersTable = () => {
 
     const [listUsers, setListUsers] = useState([]);
 
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [age, setAge] = useState("");
-    const [gender, setGender] = useState("");
-    const [address, setAddress] = useState("");
-    const [role, setRole] = useState("");
+    const [isCreateModalOpen, setIsCreateModalOpen] = useState(false);
 
-    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isUpdateModalOpen, setIsUpdateModalOpen] = useState(false);
+
 
     const access_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0b2tlbiBsb2dpbiIsImlzcyI6ImZyb20gc2VydmVyIiwiX2lkIjoiNjRkMWM0OTYxNmE3Nzc2YjExOThiZjcyIiwiZW1haWwiOiJob2lkYW5pdEBnbWFpbC5jb20iLCJhZGRyZXNzIjoiVmlldE5hbSIsImlzVmVyaWZ5Ijp0cnVlLCJuYW1lIjoiSSdtIEjhu49pIETDom4gSVQiLCJ0eXBlIjoiU1lTVEVNIiwicm9sZSI6IkFETUlOIiwiaWF0IjoxNjkzMjMyOTA2LCJleHAiOjE2OTMyOTI5MDZ9.qUR9VY8mvhsoe7LkX39fnGGYHVms2R58-DI4M14X5_0"
 
@@ -34,6 +31,7 @@ const UsersTable = () => {
         getData()
     }, [])
 
+    //Promise
     const getData = async () => {
 
         const res = await fetch(
@@ -54,7 +52,6 @@ const UsersTable = () => {
             title: 'Email',
             dataIndex: 'email',
             render: (value, record) => {
-                console.log()
                 return (<div>{record.email}</div>)
             }
         },
@@ -65,53 +62,23 @@ const UsersTable = () => {
         {
             title: 'Role',
             dataIndex: 'role',
+        },
+        {
+            title: 'Actions',
+            render: (value, record) => {
+
+                return (<div>
+                    <button onClick={() => {
+                        console.log(">>> check record: ", record)
+                        setIsUpdateModalOpen(true)
+                    }}>Edit</button>
+
+                </div>)
+            }
         }
     ]
 
-    const handleOk = async () => {
-        const data = {
-            name, email, password, age, gender, role, address
-        }
 
-        const res = await fetch(
-            "http://localhost:8000/api/v1/users",
-            {
-                method: "POST",
-                headers: {
-                    'Authorization': `Bearer ${access_token}`,
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ ...data })
-            })
-
-        const d = await res.json();
-        if (d.data) {
-            //success
-            await getData();
-            notification.success({
-                message: "Tạo mới user thành công.",
-            })
-            handleCloseCreateModal();
-        } else {
-            ///
-            notification.error({
-                message: "Có lỗi xảy ra",
-                description: JSON.stringify(d.message)
-            })
-        }
-
-    };
-
-    const handleCloseCreateModal = () => {
-        setIsModalOpen(false);
-        setName("");
-        setEmail("");
-        setPassword("");
-        setAge("");
-        setGender("");
-        setAddress("");
-        setRole("");
-    }
 
     return (
         <div>
@@ -125,7 +92,7 @@ const UsersTable = () => {
                     <Button
                         icon={<PlusOutlined />}
                         type={"primary"}
-                        onClick={() => setIsModalOpen(true)}
+                        onClick={() => setIsCreateModalOpen(true)}
                     >Add new</Button>
                 </div>
 
@@ -137,65 +104,19 @@ const UsersTable = () => {
                 rowKey={"_id"}
             />
 
-            <Modal
-                title="Add new user"
-                open={isModalOpen}
-                onOk={handleOk}
-                onCancel={() => handleCloseCreateModal()}
-                maskClosable={false}
-            >
-                <div>
-                    <label>Name:</label>
-                    <Input
-                        value={name}
-                        onChange={(event) => setName(event.target.value)}
-                    />
-                </div>
-                <div>
-                    <label>Email:</label>
-                    <Input
-                        value={email}
-                        onChange={(event) => setEmail(event.target.value)}
-                    />
-                </div>
-                <div>
-                    <label>Password:</label>
-                    <Input
-                        value={password}
-                        onChange={(event) => setPassword(event.target.value)}
-                    />
-                </div>
-                <div>
-                    <label>Age:</label>
-                    <Input
-                        value={age}
-                        onChange={(event) => setAge(event.target.value)}
-                    />
-                </div>
-                <div>
-                    <label>Gender:</label>
-                    <Input
-                        value={gender}
-                        onChange={(event) => setGender(event.target.value)}
-                    />
-                </div>
+            <CreateUserModal
+                access_token={access_token}
+                getData={getData}
+                isCreateModalOpen={isCreateModalOpen}
+                setIsCreateModalOpen={setIsCreateModalOpen}
+            />
 
-                <div>
-                    <label>Address:</label>
-                    <Input
-                        value={address}
-                        onChange={(event) => setAddress(event.target.value)}
-                    />
-                </div>
-
-                <div>
-                    <label>Role:</label>
-                    <Input
-                        value={role}
-                        onChange={(event) => setRole(event.target.value)}
-                    />
-                </div>
-            </Modal>
+            <UpdateUserModal
+                access_token={access_token}
+                getData={getData}
+                isCreateModalOpen={isUpdateModalOpen}
+                setIsCreateModalOpen={setIsUpdateModalOpen}
+            />
         </div>
     )
 }
