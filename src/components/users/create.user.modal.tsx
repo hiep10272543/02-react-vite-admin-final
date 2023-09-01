259febf1a88d20f5566e321e176eb043048e30be
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2023-09-01 14:29:42 +0700

    video 75.1

--- src/components/users/create.user.modal.tsx
+++ src/components/users/create.user.modal.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import { Modal, Input, notification } from 'antd';
-
+import { Modal, Input, notification, Button, Select, Form, InputNumber } from 'antd';
+const { Option } = Select;
 interface IProps {
     access_token: string;
     getData: any;
@@ -68,6 +68,10 @@ const CreateUserModal = (props: IProps) => {
         setRole("");
     }
 
+    const onFinish = (values: any) => {
+        console.log('Success:', values);
+    };
+
     return (
         <Modal
             title="Add new user"
@@ -76,7 +80,85 @@ const CreateUserModal = (props: IProps) => {
             onCancel={() => handleCloseCreateModal()}
             maskClosable={false}
         >
-            <div>
+            <Form
+                name="basic"
+                onFinish={onFinish}
+                layout="vertical"
+            >
+                <Form.Item
+                    label="Name"
+                    name="name"
+                    rules={[{ required: true, message: 'Please input your name!' }]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item
+                    label="Email"
+                    name="email"
+                    rules={[{ required: true, message: 'Please input your email!' }]}
+                >
+                    <Input type='email' />
+                </Form.Item>
+
+                <Form.Item
+                    label="Password"
+                    name="password"
+                    rules={[{ required: true, message: 'Please input your password!' }]}
+                >
+                    <Input.Password />
+                </Form.Item>
+                <Form.Item
+                    label="Age"
+                    name="age"
+                    rules={[{ required: true, message: 'Please input your age!' }]}
+                >
+                    <InputNumber
+                        style={{ width: "100%" }}
+                    />
+                </Form.Item>
+
+                <Form.Item
+                    label="Address"
+                    name="address"
+                    rules={[{ required: true, message: 'Please input your address!' }]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item name="gender" label="Gender" rules={[{ required: true }]}>
+                    <Select
+                        placeholder="Select a option and change input text above"
+                        // onChange={onGenderChange}
+                        allowClear
+                    >
+                        <Option value="MALE">male</Option>
+                        <Option value="FEMALE">female</Option>
+                        <Option value="OTHER">other</Option>
+                    </Select>
+                </Form.Item>
+
+                <Form.Item name="role" label="Role" rules={[{ required: true }]}>
+                    <Select
+                        placeholder="Select a option and change input text above"
+                        // onChange={onGenderChange}
+                        allowClear
+                    >
+
+                        <Option value="USER">User</Option>
+                        <Option value="ADMIN">Admin</Option>
+                    </Select>
+                </Form.Item>
+
+                <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                    <Button type="primary" htmlType="submit">
+                        Submit
+                    </Button>
+                </Form.Item>
+            </Form>
+
+
+            {/* <div>
                 <label>Name:</label>
                 <Input
                     value={name}
@@ -126,7 +208,7 @@ const CreateUserModal = (props: IProps) => {
                     value={role}
                     onChange={(event) => setRole(event.target.value)}
                 />
-            </div>
+            </div> */}
         </Modal>
     )
 }
